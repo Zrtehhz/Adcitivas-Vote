fa386f85e07a904d27b3faa419bed6ba59ef3760
Zrtehhz <beugniesjimmy@gmail.com>
2023-10-22 22:55:13 +0200

    .

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const udp = require('dgram');
 
 const config = {
-    token: 'LE-TOKEN-TOP-SERVEURS', // Token fourni par Top-Serveurs
+    token: 'EGPEDXUXDSYY', // Token fourni par Top-Serveurs
     port: 8192 // Port d'écoute UDP
 }
 
@@ -46,3 +46,38 @@ server.on('listening', function () {
 
 // On lance et on ouvre le socket
 server.bind(config.port);
+
+
+
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    // Récupération de l'adresse IP de l'utilisateur
+    fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(data => {
+        const ipAddress = data.ip;
+        document.getElementById("ip-address").innerText = ipAddress;
+      })
+      .catch(error => {
+        console.log('Erreur lors de la récupération de l\'adresse IP :', error);
+      });
+  
+    // Envoi de la requête de vote
+    document.getElementById("vote-button").addEventListener("click", function() {
+      const ip = document.getElementById("ip-address").innerText;
+      const serverToken = ":EGPEDXUXDSYY"; // Remplacer par le token fourni par Top-Serveurs
+      const voteUrl = `https://api.top-serveurs.net/v1/votes/check-ip?server_token=${serverToken}&ip=${ip}`;
+  
+      // Envoi de la requête de vote
+      fetch(voteUrl)
+        .then(response => response.json())
+        .then(data => {
+          const voteStatus = data.status;
+          document.getElementById("vote-status").innerText = "Statut du vote : " + voteStatus;
+        })
+        .catch(error => {
+          console.log('Erreur lors de la requête de vote :', error);
+        });
+    });
+  });
